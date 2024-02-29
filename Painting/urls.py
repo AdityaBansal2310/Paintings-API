16a74c4847f5ebf3ef1b1fc6158f6f352a78bf51
AdityaBansal2310 <bansaladitya842@gmail.com>
2024-02-29 23:04:46 +0530

    Fixed static media error

--- Painting/urls.py
+++ Painting/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from django.urls import path , include
 from PaintingApp.views import PaintingApiView
-# from django.conf import settings
-# from django.conf.urls.static import static
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -10,5 +10,4 @@ urlpatterns = [
     path('painting/', PaintingApiView.as_view(), name='painting-list'),
     path('painting/<int:painting_ID>/', PaintingApiView.as_view(), name='paining-detail'),
     path('api/', include('accounts.urls')),
-]
-# ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
